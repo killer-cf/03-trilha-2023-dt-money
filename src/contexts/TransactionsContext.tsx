7693ef2eca44a4a5692f7e4cae5d14442f21fb12
killer-cf <costa.kilder@live.com>
2023-03-06 15:53:06 -0300

    perf: useMemo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -64,7 +64,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
 
   useLayoutEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
